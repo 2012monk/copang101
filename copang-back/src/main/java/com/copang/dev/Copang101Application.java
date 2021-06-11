d8bf3751baaef986ead5bf26e63390d73125e190
monk <2012.monk@gmail.com>
2021-06-11 16:51:58 +0900

    init project

---
+++ copang-back/src/main/java/com/copang/dev/Copang101Application.java
@@ -0,0 +1,13 @@
+package com.copang.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Copang101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Copang101Application.class, args);
+    }
+
+}
